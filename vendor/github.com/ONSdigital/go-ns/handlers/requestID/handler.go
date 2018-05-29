df378c0c58154a43ffc22e8e0d851a50e0bf98e7
nshumoogum <nathan.shumoogum@methods.co.uk>
2018-05-29 08:01:04 +0100

    Update dependencies from go-ns

--- vendor/github.com/ONSdigital/go-ns/handlers/requestID/handler.go
+++ vendor/github.com/ONSdigital/go-ns/handlers/requestID/handler.go
@@ -2,40 +2,41 @@ package requestID
 
 import (
 	"context"
-	"math/rand"
 	"net/http"
-	"time"
-)
 
-var requestIDRandom = rand.New(rand.NewSource(time.Now().UnixNano()))
+	"github.com/ONSdigital/go-ns/common"
+)
 
 type contextKey string
 
+// ContextKey represents the constant key name
 const ContextKey = contextKey("request-id")
 
 // Handler is a wrapper which adds an X-Request-Id header if one does not yet exist
 func Handler(size int) func(http.Handler) http.Handler {
 	return func(h http.Handler) http.Handler {
-		var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
 		return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-			requestID := req.Header.Get("X-Request-Id")
+			requestID := req.Header.Get(common.RequestHeaderKey)
 
 			if len(requestID) == 0 {
-				b := make([]rune, size)
-				for i := range b {
-					b[i] = letters[requestIDRandom.Intn(len(letters))]
-				}
-				requestID = string(b)
-				req.Header.Set("X-Request-Id", requestID)
+				requestID = common.NewRequestID(size)
+				req.Header.Set(common.RequestHeaderKey, requestID)
 			}
 
-			h.ServeHTTP(w, req.WithContext(context.WithValue(req.Context(), ContextKey, requestID)))
+			ctx := context.WithValue(req.Context(), ContextKey, requestID)
+			h.ServeHTTP(w, req.WithContext(ctx))
 		})
 	}
 }
 
+// Get retrieves the value of the context key (request-id)
 func Get(ctx context.Context) string {
 	id, _ := ctx.Value(ContextKey).(string)
 	return id
+}
+
+// Set creates/overwrites the value of the context key (request-id)
+func Set(ctx context.Context, requestID string) context.Context {
+	return context.WithValue(ctx, ContextKey, requestID)
 }
