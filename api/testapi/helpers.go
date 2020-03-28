34bf9ca54668527fde31d3383a798fca1dded50d
CarlHembrough <carlhuk@gmail.com>
2020-03-28 13:10:33 +0000

    Remove redundant test log message

--- api/testapi/helpers.go
+++ api/testapi/helpers.go
@@ -8,7 +8,6 @@ import (
 	mockdatastore "github.com/ONSdigital/dp-import-api/mongo/testmongo"
 	"github.com/ONSdigital/go-ns/audit"
 	"github.com/ONSdigital/go-ns/common"
-	"github.com/ONSdigital/go-ns/log"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
@@ -50,7 +49,6 @@ func CreateRequestWithOutAuth(method, URL string, body io.Reader) (*http.Request
 func NewAuditorMock() *audit.AuditorServiceMock {
 	return &audit.AuditorServiceMock{
 		RecordFunc: func(ctx context.Context, action string, result string, params common.Params) error {
-			log.Debug("capturing audit event", nil)
 			return nil
 		},
 	}
